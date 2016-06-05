5fd9c8fd32c4dd73df26581663af8b40d65d5a4a
ivanof <ivaylo.ivanof@gmail.com>
2016-06-05 23:35:54 +0300

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,15 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private static final int width = 800;
-    private static final int high = width / 12 * 9;
+    private static final int width = 1240;  //800
+    private static final int high = 826;    //width / 12 * 9;
 
     public static void main(String[] args) {
         launch(args);
@@ -23,18 +20,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Game Nozdormu");
+
         Group root = new Group();
         Scene theScene = new Scene(root, width, high);
         primaryStage.setScene(theScene);
 
-        Canvas canvas = new Canvas(250, 250);
+        Canvas canvas = new Canvas(width, high);
         root.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Image background = new Image("imgpsh_fullsize.png");
+        Image background = new Image("test.jpg", true);
         gc.drawImage(background, width, high);
-
-
 
         primaryStage.show();
     }
