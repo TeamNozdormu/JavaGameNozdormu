32eb93c8669b081b908dbe019d789864def1d9ca
ivanof <ivaylo.ivanof@gmail.com>
2016-06-06 07:04:23 +0300

    update main

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,11 +1,12 @@
-package sample;
+package game;
 
+import game.engine.Game;
+import game.menu.Menu;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -13,6 +14,7 @@ public class Main extends Application {
 
     private static final int width = 1240;  //800
     private static final int high = 826;    //width / 12 * 9;
+    private static Menu menu;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,22 +22,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Button start = new Button("Start Game");
         primaryStage.setTitle("Game Nozdormu");
 
         Group root = new Group();
         Scene theScene = new Scene(root, width, high);
         Canvas canvas = new Canvas(width, high);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
         Image background = new Image("universe.jpg");
         Image map = new Image("map.png");
+
         gc.drawImage(background, 0, 0);
         gc.drawImage(map, 100, 100);
 
-
         root.getChildren().add(canvas);
-        root.getChildren().add(start);
+        menu = new Menu(root);
+
         primaryStage.setScene(theScene);
         primaryStage.show();
     }
