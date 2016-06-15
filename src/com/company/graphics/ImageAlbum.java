8279d6743e3b23f6077d5af1ea8ca4ad311a004c
Teodora.Nozharova <tedinoji@gmail.com>
2016-06-15 18:11:19 +0300

    New resourse files added

--- src/com/company/graphics/ImageAlbum.java
+++ src/com/company/graphics/ImageAlbum.java
@@ -3,8 +3,8 @@ package com.company.graphics;
 public enum ImageAlbum {
 
     //If you add another object to the game you must insert it in the enumeration here in order to get it's path
-    RebelPlayer("/Ships/MilleniumFalcon.png"),
-    SithPlayer("/Ships/SithInfiltrator.png"),
+    RebelPlayer("/Ships/ArcherIdle.png"),
+    SithPlayer("/Ships/ArcherIdle.png"),
     EasyEnemy("/Ships/Monster1Fly.png"),
     SturdyEnemy("/Ships/Monster2Bahamut.png"),
     DoubleDamageBonus("/InGame/x2Bonus.png"),
@@ -14,10 +14,10 @@ public enum ImageAlbum {
     GameOver("/Backgrounds/GameOver.jpg"),
     HighScores("/Backgrounds/HighScores.png"),
     Explosion("/InGame/Explosion.png"),
-    ChooseSideBG("/Backgrounds/ChooseSide.jpg"),
+    ChooseSideBG("/Backgrounds/ChoosePlayer.jpg"),
     MainMenu("/Backgrounds/MainMenu.jpg"),
-    Button("/Interface/Button.png"),
-    ButtonSlider("/Interface/ButtonSlider.jpg");
+    Button("/Interface/ButtonGreen.png"),
+    ButtonSlider("/Interface/IvySlider.png");
 
     private String path;
 
