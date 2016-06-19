546761cb410b61ef1f4ba3a06f39ce29040c9be6
ivanof <ivaylo.ivanof@gmail.com>
2016-06-19 17:42:54 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import com.company.game.Game;
 public class Main {
     public static void main(String[] args) {
         //  State state = new MainMenuState();
-        // TODO: Load from file
+        // TODO: Load from file - ?
         Game game = new Game();
         game.start();
     }
