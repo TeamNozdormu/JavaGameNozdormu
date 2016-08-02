64a939e4cafc1a56fe560c841c169000acf3008a
ivanof <ivaylo.ivanof@gmail.com>
2016-08-02 04:25:27 +0300

    create and using getters and setters;

--- src/com/company/gameobjects/Game.java
+++ src/com/company/gameobjects/Game.java
@@ -18,8 +18,8 @@ public class Game implements Runnable {
     private MouseInput mouseInput;
     private Thread thread;
     private KeyboardInput keyboardInput;
-    private BufferStrategy bs;
-    private Graphics g;
+    private BufferStrategy bufferStrategy;
+    private Graphics graphics;
     private State gameState;
     private State menuState;
     private State gameOverState;
@@ -33,52 +33,166 @@ public class Game implements Runnable {
         return this.isRunning;
     }
 
+    private GameMap getMap() {
+        return map;
+    }
+
+    private void setMap(GameMap map) {
+        this.map = map;
+    }
+
+    private Display getDisplay() {
+        return display;
+    }
+
+    private void setDisplay(Display display) {
+        this.display = display;
+    }
+
+    private MouseInput getMouseInput() {
+        return mouseInput;
+    }
+
+    private void setMouseInput(MouseInput mouseInput) {
+        this.mouseInput = mouseInput;
+    }
+
+    private Thread getThread() {
+        return thread;
+    }
+
+    private void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+    private KeyboardInput getKeyboardInput() {
+        return keyboardInput;
+    }
+
+    private void setKeyboardInput(KeyboardInput keyboardInput) {
+        this.keyboardInput = keyboardInput;
+    }
+
+    private BufferStrategy getBufferStrategy() {
+        return bufferStrategy;
+    }
+
+    private void setBufferStrategy(BufferStrategy bufferStrategy) {
+        this.bufferStrategy = bufferStrategy;
+    }
+
+    private Graphics getGraphics() {
+        return graphics;
+    }
+
+    private void setGraphics(Graphics graphics) {
+        this.graphics = graphics;
+    }
+
+    private State getGameState() {
+        return gameState;
+    }
+
+    private void setGameState(State gameState) {
+        this.gameState = gameState;
+    }
+
+    private State getMenuState() {
+        return menuState;
+    }
+
+    private void setMenuState(State menuState) {
+        this.menuState = menuState;
+    }
+
+    private State getGameOverState() {
+        return gameOverState;
+    }
+
+    private void setGameOverState(State gameOverState) {
+        this.gameOverState = gameOverState;
+    }
+
+    private State getHighScoreState() {
+        return highScoreState;
+    }
+
+    private void setHighScoreState(State highScoreState) {
+        this.highScoreState = highScoreState;
+    }
+
+    private State getChooseDifficulty() {
+        return ChooseDifficulty;
+    }
+
+    private void setChooseDifficulty(State chooseDifficulty) {
+        ChooseDifficulty = chooseDifficulty;
+    }
+
+    private State getChooseSideState() {
+        return chooseSideState;
+    }
+
+    private void setChooseSideState(State chooseSideState) {
+        this.chooseSideState = chooseSideState;
+    }
+
+    private State getIntroState() {
+        return IntroState;
+    }
+
+    private void setIntroState(State introState) {
+        IntroState = introState;
+    }
+
+    private State getIntroTaskState() {
+        return IntroTaskState;
+    }
+
+    private void setIntroTaskState(State introTaskState) {
+        IntroTaskState = introTaskState;
+    }
+
     public void setRunning(boolean running) {
         this.isRunning = running;
     }
 
     private void init() {
-        this.map = new GameMap(
-                GameSettings.GAME_WIDTH,
-                GameSettings.GAME_HEIGHT);
-        this.display = new Display(
-                GameSettings.GAME_NAME,
-                GameSettings.GAME_WIDTH,
-                GameSettings.GAME_HEIGHT);
-        this.keyboardInput = new KeyboardInput(this, this.display);
-        this.mouseInput = new MouseInput(this.display);
-        this.gameState = new GameState();
-        this.menuState = new MainMenuState();
-        this.gameOverState = new GameOverState();
-        this.highScoreState = new HighScoresState("Descending");
-        this.ChooseDifficulty = new ChooseDifficultyState();
-        this.chooseSideState = new ChooseSideState();
-        this.IntroState = new IntroState();
-        this.IntroTaskState = new IntroTaskState();
-        StateManager.setCurrentState(this.menuState);
+        this.setMap(new GameMap(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT));
+        this.setDisplay(new Display(GameSettings.GAME_NAME, GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT));
+        this.setKeyboardInput(new KeyboardInput(this, this.display));
+        this.setMouseInput(new MouseInput(this.display));
+        this.setGameState(new GameState());
+        this.setMenuState(new MainMenuState());
+        this.setChooseDifficulty(new ChooseDifficultyState());
+        this.setChooseSideState(new ChooseSideState());
+        this.setIntroState(new IntroState());
+        this.setIntroTaskState(new IntroTaskState());
+        this.setGameOverState(new GameOverState());
+        this.setHighScoreState(new HighScoresState("Descending"));
+        StateManager.setCurrentState(this.getMenuState());
     }
 
     public void displayFrame() {
         //Display the current frame using the display frame;
-        this.bs = this.display.getCanvas().getBufferStrategy();
+        this.setBufferStrategy(this.display.getCanvas().getBufferStrategy());
 
-        if (bs == null) {
+        if (this.getBufferStrategy() == null) {
             this.display.getCanvas().createBufferStrategy(2);
             return;
         }
 
-        this.g = bs.getDrawGraphics();
+        this.setGraphics(bufferStrategy.getDrawGraphics());
         /////////DRAW HERE /////////////////
-
-        g.clearRect(0, 0, GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT);
+        this.getGraphics().clearRect(0, 0, GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT);
 
         if (StateManager.getCurrentState() != null) {
-            StateManager.getCurrentState().display(g);
+            StateManager.getCurrentState().display(graphics);
         }
 
         ////////////// TO HERE////////////
-        g.dispose();
-        bs.show();
+        this.getGraphics().dispose();
+        this.getBufferStrategy().show();
     }
 
     public void update() {
@@ -93,8 +207,8 @@ public class Game implements Runnable {
         }
 
         this.isRunning = true;
-        this.thread = new Thread(this);
-        this.thread.start();
+        this.setThread(new Thread(this));
+        this.getThread().start();
     }
 
     public synchronized void stop() {
@@ -105,7 +219,7 @@ public class Game implements Runnable {
         this.isRunning = false;
 
         try {
-            this.thread.join();
+            this.getThread().join();
         } catch (InterruptedException e) {
             e.printStackTrace();
             System.exit(1);
