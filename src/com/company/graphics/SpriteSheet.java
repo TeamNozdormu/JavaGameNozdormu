7714eafa752b6f557d9457fd6afbe78d3be6ecb2
ivanof <ivaylo.ivanof@gmail.com>
2016-08-03 20:05:33 +0300

    add video with my game

--- src/com/company/graphics/SpriteSheet.java
+++ src/com/company/graphics/SpriteSheet.java
@@ -9,14 +9,38 @@ public class SpriteSheet {
     private int cropHeight;
 
     public SpriteSheet(BufferedImage sheet, int cropWidth, int cropHeight) {
+        this.setSheet(sheet);
+        this.setCropWidth(cropWidth);
+        this.setCropHeight(cropHeight);
+    }
+
+    public BufferedImage getSheet() {
+        return sheet;
+    }
+
+    private void setSheet(BufferedImage sheet) {
         this.sheet = sheet;
+    }
+
+    public int getCropWidth() {
+        return cropWidth;
+    }
+
+    private void setCropWidth(int cropWidth) {
         this.cropWidth = cropWidth;
+    }
+
+    public int getCropHeight() {
+        return cropHeight;
+    }
+
+    private void setCropHeight(int cropHeight) {
         this.cropHeight = cropHeight;
     }
 
     //Method that crops the image from x to width and from y to height
     public BufferedImage crop(int x, int y) {
         //Returns a new image in the area we specified
-        return sheet.getSubimage(x * this.cropWidth, y * this.cropHeight, this.cropWidth, this.cropHeight);
+        return sheet.getSubimage(x * this.getCropWidth(), y * this.getCropHeight(), this.getCropWidth(), this.getCropHeight());
     }
 }
